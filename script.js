fc72ac7171fe50034b32d437a352111ab27f829f
Faizan Ahmed <153298469+Faizan-fzn@users.noreply.github.com>
2024-02-04 23:19:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,131 +1,73 @@
+const form = document.getElementById("task-form");
+const model = document.getElementById("model");
+const closeBtn = document.getElementById("cloes-btn");
+const addBtn = document.getElementById("addBtn");
+const taskList = {
+    TODO:[], COMPLETED:[] , STARTED:[]
+};
+let priorityCnt = [0,0,0];
+let taskCnt = [0,0,0];
 
-body{
-    background-color: #191919;
- }
- .logo{
-    display: flex;
-    width: 100%;
-    height: 200px;
-    align-items: center;
-    justify-content: center;
- }
- .searchBar{
-    display: flex;
-    align-items: center;
-    gap:5px;
- }
- .searchBar span{
-    color: white;
-    font-size: xx-large;
-    padding-right:20px;
- }
- #search-bar{
-     Width: 644px;
-     Height: 54px;
-     background-color: #262626;
-     border-radius: 8px;
-     color: #808080;
-     font-size: larger;
- }
- .btn{
-     background-color: #1E6F9F;
-     width: 90px;
-     height: 54px;
-     border-radius: 8px;
-     color: white;
-     border: none;
-     cursor: pointer;
- }
- .navBar{
-     display: flex;
-     justify-content: center;
-     gap: 8px;
-     margin-top: -30px;
- }
-.close-btn{
-    background-color: red;
-    font-weight: 300;
-    height: 30px;
+function toggleModel(){
+    model.classList.toggle("hide");
+    model.classList.toggle("show");
 }
-#submit-btn{
-    height: 30px;
-}
-.form-btn{
-    display: flex;
-    justify-content: center;
-    gap:30px;
-}
-form {
-    border: 1px solid #808080;
-    width: 60vw;
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    padding: 20px;
-    z-index: 10;
-    background-color: #808080;
-}
-.show{
-    display: flex;
-    justify-content: center;
-    padding: 30px;
-    margin-top: 10px;
-}
-.hide{
-    display: none;
-}
-.container{
-    width: 600px;
-    height: 55px;
-    display: flex;
-    justify-content: space-between;
-    margin-top: 50px;
-}
-.To-Do-1{
-   color: #1e6f9f;
-   font:Inter;
-   font-weight: 700;
-   font-size: 14px;
+addBtn.addEventListener("click" , toggleModel);
+closeBtn.addEventListener("click" , toggleModel);
+form.addEventListener("submit" , (e) => {
+    e.preventDefault();
+    const taskStatus = form.status.value;
+    
+    const taskInfo = {
+        title : form.title.value,
+        date : form.date.value,
+        priority : form.priority.value,
+    }
 
-}
-.High-1{
-    color: #8284fa;
-    font-weight: 700;
-    font-size:14px;
-    font:Inter;
-    justify-content: space-between;
-}
-.count{
-    color: white;
-    background-color: #333333;
-    padding: 3px 9px;
-    border-radius: 12px;
-}
-main{
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    margin: auto;
-    grid-template-columns: repeat(3,1fr);
-    gap:10px;
-    padding:10px;
-}
-.card{
-    display: flex;
-    justify-content: space-between;
-    background-color: #262626;
-    align-items: center;
-    width: 570px;
-    height: 50px;
-    gap: 5px;
-    padding: 10px;
-    box-shadow: 0px 2px 8px 0px #0000000F;
-    border-radius: 8px;
-    color: rgba(255, 255, 255, 0.83);
-}
-.task-holder{
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    justify-content: center;
-}
+    taskList[taskStatus].push(taskInfo);
+    const taskContainer = document.getElementById(taskStatus);
+    if(taskStatus === "TODO"){
+        if(taskInfo.priority==="HIGH"){
+            priorityCnt[0] = priorityCnt[0]+1;
+        };
+        taskCnt[0] = taskCnt[0]+1;
+        
+        taskContainer.children[0].children[0].children[1].innerText = `${taskCnt[0]}`;
+        taskContainer.children[0].children[1].children[1].innerText = `${priorityCnt[0]}`;
+    }else if(taskStatus === "STARTED"){
+        if(taskInfo.priority==="HIGH"){
+            priorityCnt[1] = priorityCnt[1]+1;
+        };
+        taskCnt[1] = taskCnt[1]+1;
+        taskContainer.children[0].children[0].children[1].innerText = `${taskCnt[1]}`;
+        taskContainer.children[0].children[1].children[1].innerText = `${priorityCnt[1]}`;
+    }else{
+        if(taskInfo.priority==="HIGH"){
+            priorityCnt[2] = priorityCnt[2]+1;
+        };
+        taskCnt[2] = taskCnt[2]+1;
+        taskContainer.children[0].children[0].children[1].innerText = `${taskCnt[2]}`;
+        taskContainer.children[0].children[1].children[1].innerText = `${priorityCnt[2]}`;
+    }
+    //         <div class="card">
+    //             <div id="title">Lorem ipsum dolorvoluptatum seditis reiciendis.</div>
+    //             <div id="date">12-12-2024</div>
+    //             <div id="card-btn">
+    //                 <span class="material-symbols-outlined" id="deleteBtn">edit</span>
+    //                 <span class="material-symbols-outlined" id="editBtn">delete</span>
+    //             </div>
+    //         </div>
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+            <h4>${taskInfo.title}</h4>
+            <div id="date">${taskInfo.date}</div>
+            <div id="card-btn">
+                <span class="material-symbols-outlined">edit</span>
+                <span class="material-symbols-outlined">delete</span>
+            </div>
+                    `;
+    taskContainer.appendChild(card);
+    form.reset();
+    toggleModel();
+});
